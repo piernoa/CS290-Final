9d6e7d2af67cbf70ba1ef3fee871b8da23dc4a75
Andrew Pierno <piernoa@onid.oregonstate.edu>
2015-05-31 11:23:32 -0700

    upgraded to 1.3, fixed breaking changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,14 @@
-
-
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('myApp', ['ngRoute', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
-    $routeProvider.when('/project_a', {templateUrl: 'partials/project_a.html', controller: GenericViewCtrl});
-    $routeProvider.when('/project_b', {templateUrl: 'partials/project_b.html', controller: GenericViewCtrl});
-    $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
-    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: LoginCtrl});
-    $routeProvider.when('/signup', {templateUrl: 'partials/signup.html', controller: SignupCtrl});
+    $routeProvider.when('/home', {templateUrl: 'partials/home.html' , controller: "GenericViewCtrl"});
+    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: "LoginCtrl"});
+    $routeProvider.when('/signup', {templateUrl: 'partials/signup.html', controller: "SignupCtrl"});
+
     // secure
-    $routeProvider.when('/app/create', {templateUrl: 'partials/secure/create.html', controller: CreateCtrl});
-    $routeProvider.when('/app/list', {templateUrl: 'partials/secure/list.html', controller: ListCtrl});
+    $routeProvider.when('/app/create', {templateUrl: 'partials/secure/create.html', controller: "CreateCtrl"});
+    $routeProvider.when('/app/list', {templateUrl: 'partials/secure/list.html', controller: "ListCtrl"});
 
     // default
-    $routeProvider.otherwise({redirectTo: '/home'});
-  }]);
+     $routeProvider.otherwise({redirectTo: '/home'});
+  }])
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,7 +3,7 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
+angular.module('myApp.directives', ['ngRoute']).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
