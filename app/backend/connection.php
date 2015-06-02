3b7390769c11fbfeaf3596fea5949b952168fc94
Andrew Pierno <piernoa@onid.oregonstate.edu>
2015-06-01 17:47:50 -0700

    switched connection to/from local

--- app/backend/connection.php
+++ app/backend/connection.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-//$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "piernoa-db", "SZ31zlLRwwV6wyUh", "piernoa-db");
-$mysqli = new mysqli("localhost", "root", "root", "school");
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "piernoa-db", "SZ31zlLRwwV6wyUh", "piernoa-db");
+//$mysqli = new mysqli("localhost", "root", "root", "school");
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
